327f63735f0e4723dde0d354b43680b4a71812a7
Penca53 <pencapencator@gmail.com>
2021-07-23 21:28:22 +0200

    Initial project setup.

---
+++ src/main/java/com/cdl/inventory_manager_locations_service/InventoryManagerLocationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cdl.inventory_manager_locations_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerLocationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagerLocationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdl/inventory_manager_locations_service/InventoryManagerLocationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdl.inventory_manager_locations_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerLocationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
